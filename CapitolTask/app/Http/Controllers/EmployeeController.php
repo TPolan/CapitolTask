bb369090a6328a053bdd1ff77b3c803e0f540be7
TPolan <tpolan47@gmail.com>
2020-05-07 14:21:02 +0200

    create controller, add routes

---
+++ CapitolTask/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function all()
+    {
+
+        return;
+    }
+
+    public function allWith(Request $request)
+    {
+        return;
+    }
+
+    public function allWithout(Request $request)
+    {
+        return;
+    }
+}
--- CapitolTask/routes/web.php
+++ CapitolTask/routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/employees', 'all@EmployeeController');
+Route::get('/employees/{withJobId}', 'allWith@EmployeeController');
+Route::get('/employees/{withoutJobId}', 'allWithout@EmployeeController');
+
