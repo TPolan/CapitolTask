0128c747a7fd6556e8b775a1fba79e5de4e4d6cb
TPolan <tpolan47@gmail.com>
2020-05-07 15:44:57 +0200

    refactoring controller

--- CapitolTask/app/Http/Controllers/EmployeeController.php
+++ CapitolTask/app/Http/Controllers/EmployeeController.php
@@ -2,23 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Job;
+use App\Employee;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    public function all()
+    public function all(Request $request): JsonResponse
     {
+        if ($request->with) {
+            dd('with');
+            /** @var Job $jobs */
+            $jobs = Job::query()->where('job_code', $request->with)->get();
+            return response()->json($jobs->employees()->get());
+        }
 
-        return;
-    }
+        if ($request->without) {
+            dd('without');
+            /** @var Job $jobs */
+            $jobs = Job::query()->where('job_code', $request->without)->get();
+            return response()->json($jobs->employees()->get());
+        }
 
-    public function allWith(Request $request)
-    {
-        return;
-    }
-
-    public function allWithout(Request $request)
-    {
-        return;
+        return response()->json(Employee::all());
     }
 }
--- CapitolTask/routes/web.php
+++ CapitolTask/routes/web.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/employees', 'all@EmployeeController');
-Route::get('/employees/{withJobId}', 'allWith@EmployeeController');
-Route::get('/employees/{withoutJobId}', 'allWithout@EmployeeController');
+Route::get('/employees/list', 'EmployeeController@all');
+
 
